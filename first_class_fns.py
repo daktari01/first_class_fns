7b648c8bf480d75541fdb93dd0b990a7c09b4cef
James Dindi <dindijjames@gmail.com>
2017-10-17 05:39:38 +0300

    Finally, on first class functions

--- first_class_fns.py
+++ first_class_fns.py
@@ -1,26 +1,15 @@
-def logger(msg):
+
+def html_tag(tag):
     
-    def log_message():
-        print('Log:', msg)
-    return log_message
+    def wrap_text(msg):
+        print('<{0}>{1}</{0}>'.format(tag, msg))
 
-log_hi = logger('Hi')
-log_hi()
+    return wrap_text
 
-"""def square(x):
-    return x* x
+print_h1 = html_tag('h1')
+print_h1('Test Headline!')
+print_h1('Another Headline!')
 
-def cube(x):
-    return x * x * x
+print_p = html_tag('p')
+print_p('Test paragraph')
 
-def my_map(func, arg_list):
-    result = []
-    #result = [result.append(func(i) for i in arg_list)]
-    for i in arg_list:
-        result.append(func(i))
-    return result
-
-squares = my_map(square, [1, 2, 3, 4, 5])
-
-print(squares)
-"""
