46ff53f483c245d7188a0da91f03381a523ce43c
James Dindi <dindijjames@gmail.com>
2017-10-18 07:12:46 +0300

    Closures part 2

--- closures.py
+++ closures.py
@@ -1,11 +1,13 @@
-def outer_func():
-    message = 'Hi'
+def outer_func(msg):
+    message = msg
 
     def inner_func():
         print(message)
 
     return inner_func
 
-my_func = outer_func()
+hi_func = outer_func('Hi')
+hello_func = outer_func('Hello')
 
-my_func()
+hi_func()
+hello_func()
