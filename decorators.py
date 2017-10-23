fc1328d8553e58e26f34bab7af2cf90dbf39eab6
James Dindi <dindijjames@gmail.com>
2017-10-23 05:44:15 +0300

    Decorators in practical situations

--- decorators.py
+++ decorators.py
@@ -1,27 +1,36 @@
-def decorator_funtion(original_function):
-    def wrapper_function(*args, **kwargs):
-        print('wrapper executed this before {}'.format(original_function.__name__))
-        return original_function(*args, **kwargs)
-    return wrapper_function
+#decorators in practical
+def my_logger(original_func):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(original_func.__name__), level = logging.INFO)
 
-class decorator_class(object):
-    def __init__(self, original_function):
-        self.original_function = original_function
+    def wrapper(*args, **kwargs):
+        logging.info(
+            'Ran with args: {}, and kwargs: {}'.format(args, kwargs)
+        )
+        return original_func(*args, **kwargs)
 
-    def __call__(self, *args, **kwargs):
-        print('call method executed this before {}'.format(self.original_function.__name__))
-        return self.original_function(*args, **kwargs)
+    return wrapper
+
+def my_timer(orig_func):
+    import time
+
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = orig_func(*args, **kwargs)
+        t2 = time.time() - t1
+        print('{} ran in: {} sec'.format(orig_func.__name__, t2))
+        return result
+
+    return wrapper
         
 
-@decorator_class
+'''@decorator_function
 def display():
-    print('display function ran')
+    print('display function ran')'''
 
-@decorator_class
+@my_logger
 def display_info(name, age):
     print('display_info ran with arguments ({}, {})'.format(name, age))
 
 
 display_info('John', 25)
-
-display()
